bfe564e1dbe12f054cdd08081f396c761be0342f
placido_penitente <antaresplacido@gmail.com>
2020-10-14 20:42:54 +0800

    Targeting Script Implimentation

---
+++ Assets/Scripts/Enemy/EnemyShoot.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyShoot : MonoBehaviour {
+
+	public GameObject PlayerTarget;
+	public Transform PlayerTransform;
+	public GameObject BulletPrefab;
+
+	// Use this for initialization
+	void Start () {
+		
+		PlayerTarget = GameObject.FindGameObjectWithTag("Player");
+		PlayerTransform = PlayerTarget.GetComponent<Transform> ();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+	}
+
+	void LookAtTarget()
+	{
+		transform.LookAt(PlayerTransform);
+	}
+}
